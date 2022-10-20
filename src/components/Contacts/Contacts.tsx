9a965e0418155957af3f49b2129a93367fb5f059
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-20 17:45:42 +0300

    feat: add id to Contacts component

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -3,7 +3,7 @@ import classes from './Contacts.module.scss';
 
 export const Contacts = () => {
     return (
-        <div className={classes.contacts}>
+        <div className={classes.contacts} id='contacts'>
             <div className={classes.container}>
                 <div className={classes.left}>
                     <div className={classes.description}>
@@ -16,7 +16,7 @@ export const Contacts = () => {
                     <p className={classes.address}>Dnipro, Ukraine</p>
                     <p className={classes.email}>a.karpukhin9@gmail.com</p>
                     <ul className={classes.list}>
-                        <li className={classes.link}><a href="#home">Home</a></li>
+                        <li className={classes.link}><a href="#main">Home</a></li>
                         <li className={classes.link}><a href="#about">About</a></li>
                         <li className={classes.link}><a href="#projects">Projects</a></li>
                     </ul>
