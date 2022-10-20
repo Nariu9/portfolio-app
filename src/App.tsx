ff90a50ac1ddcad322393138d00654823dc2beda
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-20 17:44:20 +0300

    feat: import AOS to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.scss';
 import {Navbar} from './components/Navbar/Navbar';
 import {Main} from './components/Main/Main';
@@ -6,8 +6,16 @@ import {About} from './components/About/About';
 import {Footer} from './components/Footer/Footer';
 import {Projects} from './components/Projects/Projects';
 import {Contacts} from './components/Contacts/Contacts';
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 function App() {
+
+    useEffect(()=>{
+        AOS.init()
+        AOS.refresh()
+    })
+
     return (
         <div className="App">
             <Navbar/>
