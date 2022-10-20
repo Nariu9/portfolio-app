99a456a9e08c08a2d2fd2af1c2d8ca01c1358260
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-20 15:36:13 +0300

    feat: import components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import './App.scss';
 import {Navbar} from './components/Navbar/Navbar';
+import {Main} from './components/Main/Main';
+import {About} from './components/About/About';
+import {Footer} from './components/Footer/Footer';
+import {Projects} from './components/Projects/Projects';
+import {Contacts} from './components/Contacts/Contacts';
 
 function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar/>
+            <Main/>
+            <About/>
+            <Projects/>
+            <Contacts/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
