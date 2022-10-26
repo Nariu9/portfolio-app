f5cdd1e3d69d93bcb4b4df10dc12bc64317d0fd7
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-26 22:29:32 +0300

    feat: implement Project component

---
+++ src/components/Projects/project/Project.tsx
@@ -0,0 +1,25 @@
+import React, {FC} from 'react';
+import classes from './Project.module.scss';
+
+type ProjectType = {
+    style: { backgroundImage: string }
+    code: string
+    demo: string
+    title: string
+    description: string
+}
+
+export const Project: FC<ProjectType> = ({style, code, demo, title, description}) => {
+    return (
+        <div className={classes.project}>
+            <div className={classes.imageBlock} style={style}>
+                <a className={classes.link} href={code}>Code</a>
+                <a className={classes.link} href={demo}>Demo</a>
+            </div>
+            <div className={classes.descriptionBlock}>
+                <h3 className={classes.projectTitle}>{title}</h3>
+                <span className={classes.description}>{description}</span>
+            </div>
+        </div>
+    );
+};
