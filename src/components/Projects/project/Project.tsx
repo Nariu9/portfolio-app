16d602a333602519ff18712d8fabedeb20a8799a
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-27 16:51:38 +0300

    feat: implement a redirect to a new page when clicking on a link

--- src/components/Projects/project/Project.tsx
+++ src/components/Projects/project/Project.tsx
@@ -13,8 +13,8 @@ export const Project: FC<ProjectType> = ({style, code, demo, title, description}
     return (
         <div className={classes.project}>
             <div className={classes.imageBlock} style={style}>
-                <a className={classes.link} href={code}>Code</a>
-                <a className={classes.link} href={demo}>Demo</a>
+                <a className={classes.link} href={code} target={'_blank'} rel={'noreferrer'}>Code</a>
+                <a className={classes.link} href={demo} target={'_blank'} rel={'noreferrer'}>Demo</a>
             </div>
             <div className={classes.descriptionBlock}>
                 <h3 className={classes.projectTitle}>{title}</h3>
