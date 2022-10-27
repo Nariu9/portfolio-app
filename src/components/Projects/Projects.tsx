ada87f3072aae6abadbad55208d8da2e33b5c88a
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-27 16:54:30 +0300

    feat: update Projects component

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,25 +1,68 @@
 import React from 'react';
 import classes from './Projects.module.scss';
-import cards from '../../images/social.webp'
-import todo from '../../images/todo.png'
-import socialNetwork from '../../images/social.webp'
+import cards from '../../assets/images/Cards.png'
+import todo from '../../assets/images/TodoList.png'
+import socialNetwork from '../../assets/images/SocialNetwork.png'
+import {Zoom} from 'react-awesome-reveal';
+import {Project} from './project/Project';
+import {Title} from '../../common/components/Title/Title';
+import {HiOutlineEnvelope} from 'react-icons/hi2';
 
 export const Projects = () => {
+
+    const content = [
+        {
+            id: 0,
+            style: {
+                backgroundImage: `url(${cards})`,
+            },
+            code: 'https://github.com/ticket1201/cards_quiz',
+            demo: 'https://ticket1201.github.io/cards_quiz/',
+            title: 'Card Quiz',
+            description: 'An application for consolidating existing knowledge or learning something new'
+        },
+        {
+            id: 1,
+            style: {
+                backgroundImage: `url(${todo})`,
+            },
+            code: 'https://github.com/Nariu9/todolist-ts',
+            demo: 'https://nariu9.github.io/todolist-ts/',
+            title: 'To-do List',
+            description: 'Reflection of those things that need to be done, bought or just remembered'
+        },
+        {
+            id: 2,
+            style: {
+                backgroundImage: `url(${socialNetwork})`,
+            },
+            code: 'https://github.com/Nariu9/social-network-ts',
+            demo: 'https://nariu9.github.io/social-network-ts/',
+            title: 'Social Network',
+            description: 'A place to post some information and make new connections'
+        }
+    ]
+
     return (
         <div className={classes.projects} id="projects">
             <div className={classes.container}>
-                <div className={classes.left} data-aos="fade-right">
-                    <p className={classes.title}>My latest works</p>
-                    <p className={classes.subtitle}>Perfect solutions for digital experience
-                    </p>
-                    <img src={cards} alt="cards" className={classes.projectImg}/>
-                    <button className={classes.button}>All projects</button>
-                    <p className={classes.extra}>Some projects not allowed to publish by NDA, if you want to see
-                        more, <a href="#contacts">contact me</a></p>
+                <Title title={'My latest works'}/>
+                <p className={classes.subtitle}>The presented projects were developed by me personally or as part of a
+                    team. They can still be improved in many ways.</p>
+                <div className={classes.wrapper}>
+                    <Zoom cascade damping={0.2}>
+                        <Project style={content[0].style} code={content[0].code} demo={content[0].demo}
+                                 title={content[0].title} description={content[0].description}/>
+                        <Project style={content[1].style} code={content[1].code} demo={content[1].demo}
+                                 title={content[1].title} description={content[1].description}/>
+                        <Project style={content[2].style} code={content[2].code} demo={content[2].demo}
+                                 title={content[2].title} description={content[2].description}/>
+                    </Zoom>
                 </div>
-                <div className={classes.right} data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="500">
-                    <img src={todo} alt="todoList" className={classes.projectImg}/>
-                    <img src={socialNetwork} alt="socialNetwork" className={classes.projectImg}/>
+                <div className={classes.info}>
+                    <p className={classes.extra}>If you want to see more, feel free to contact me any time
+                    </p>
+                    <a href="mailto:a.karpukhin9@gmail.com"><HiOutlineEnvelope className={classes.email}/></a>
                 </div>
             </div>
         </div>
