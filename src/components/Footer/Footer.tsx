732251497753559a5f8813ace57c3b7c7559c50d
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-28 23:14:32 +0300

    refactor: update Footer structure

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,25 +1,13 @@
 import React from 'react';
 import classes from './Footer.module.scss';
-import linkedIn from '../../images/linkedin.svg'
-import telegram from '../../images/telegram.svg'
-import github from '../../images/github.svg'
-import codeWars from '../../images/codewars.svg'
 
 export const Footer = () => {
     return (
-        <div className={classes.footer}>
-            <div className={classes.rights}>
-                <p className={classes.name}>
-                    <span>Artem</span>Karpukhin
-                </p>
-                <p className={classes.copyRights}>&#169; 2022 All rights reserved</p>
+        <footer className={classes.footer}>
+            <div className={classes.container}>
+                <p className={classes.thanks}>Thanks for scrolling</p>
+                <p>&#169; 2022 All rights reserved</p>
             </div>
-            <div className={classes.socials}>
-                <img src={linkedIn} alt="linkedIn" className={classes.socialIcon}/>
-                <img src={telegram} alt="telegram" className={classes.socialIcon}/>
-                <img src={github} alt="github" className={classes.socialIcon}/>
-                <img src={codeWars} alt="codeWars" className={classes.socialIcon}/>
-            </div>
-        </div>
+        </footer>
     );
 };
