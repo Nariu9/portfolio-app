1ad9ad580843c99dbc9220274f5ca9b69bd79bb1
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-28 23:13:38 +0300

    refactor: update Contacts structure

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,25 +1,30 @@
 import React from 'react';
 import classes from './Contacts.module.scss';
+import {Title} from '../../common/components/Title/Title';
+import {BsGithub, BsLinkedin, BsTelegram} from 'react-icons/bs';
+import {SiCodewars} from 'react-icons/si';
+import {FaEnvelope} from 'react-icons/fa';
+import {ContactsForm} from './ContactsForm/ContactsForm';
 
 export const Contacts = () => {
     return (
-        <div className={classes.contacts} id='contacts'>
+        <div className={classes.contacts} id="contacts">
             <div className={classes.container}>
-                <div className={classes.left}>
-                    <div className={classes.description}>
-                        Let's create something amazing together
+                <Title title={'Let\'s create something amazing together'}/>
+                <div className={classes.wrapper}>
+                    <div className={classes.socials}>
+                        <p className={classes.subtitle}>How about <span>say hi ? </span>Contact me via socials or just fill out the form.</p>
+                        <div className={classes.icons}>
+                            <a href="https://t.me/tema_213" target={'_blank'} rel={'noreferrer'}><BsTelegram/></a>
+                            <a href="https://github.com/Nariu9" target={'_blank'} rel={'noreferrer'}><BsGithub/></a>
+                            <a href="https://www.linkedin.com/in/artem-karpukhin/" target={'_blank'} rel={'noreferrer'}><BsLinkedin/></a>
+                            <a href="https://www.codewars.com/users/Nariu9" target={'_blank'} rel={'noreferrer'}><SiCodewars/></a>
+                            <a href="mailto:a.karpukhin9@gmail.com"><FaEnvelope/></a>
+                        </div>
                     </div>
-                    <p className={classes.connect}>Start by <span>saying hi</span></p>
-                </div>
-                <div className={classes.right}>
-                    <p className={classes.title}>Information</p>
-                    <p className={classes.address}>Dnipro, Ukraine</p>
-                    <p className={classes.email}>a.karpukhin9@gmail.com</p>
-                    <ul className={classes.list}>
-                        <li className={classes.link}><a href="#main">Home</a></li>
-                        <li className={classes.link}><a href="#about">About</a></li>
-                        <li className={classes.link}><a href="#projects">Projects</a></li>
-                    </ul>
+                    <div className={classes.message}>
+                        <ContactsForm/>
+                    </div>
                 </div>
             </div>
         </div>
