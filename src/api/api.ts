a773801b55f515c349263590e0828cf4456d69ed
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-28 23:18:14 +0300

    feat: implement emailjs api

---
+++ src/api/api.ts
@@ -0,0 +1,33 @@
+import emailjs from '@emailjs/browser';
+import {FormType} from '../components/Contacts/ContactsForm/ContactsForm';
+import {toast} from 'react-toastify';
+
+
+export const api = {
+    sendEmail(data: Required<FormType>) {
+        return emailjs.send('service_x1g04sz', 'template_hajqy3c', data, 'D1uooWrn5oFlP4B2H')
+            .then((result) => {
+                toast.success('Message sent! I will contact you as soon as possible.', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'dark',
+                });
+            }, (error) => {
+                toast.error('Something went wrong, please try again later.', {
+                    position: 'top-right',
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: 'dark',
+                });
+            });
+    }
+}
