984f5880aaa7b6729c2278968c42480fd491868c
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-29 22:55:58 +0300

    refactor: remove unused div

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -13,18 +13,18 @@ export const Contacts = () => {
                 <Title title={'Let\'s create something amazing together'}/>
                 <div className={classes.wrapper}>
                     <div className={classes.socials}>
-                        <p className={classes.subtitle}>How about <span>say hi ? </span>Contact me via socials or just fill out the form.</p>
+                        <p className={classes.subtitle}>How about <span>say hi ? </span>Contact me via socials or just
+                            fill out the form.</p>
                         <div className={classes.icons}>
                             <a href="https://t.me/tema_213" target={'_blank'} rel={'noreferrer'}><BsTelegram/></a>
                             <a href="https://github.com/Nariu9" target={'_blank'} rel={'noreferrer'}><BsGithub/></a>
                             <a href="https://www.linkedin.com/in/artem-karpukhin/" target={'_blank'} rel={'noreferrer'}><BsLinkedin/></a>
-                            <a href="https://www.codewars.com/users/Nariu9" target={'_blank'} rel={'noreferrer'}><SiCodewars/></a>
+                            <a href="https://www.codewars.com/users/Nariu9" target={'_blank'}
+                               rel={'noreferrer'}><SiCodewars/></a>
                             <a href="mailto:a.karpukhin9@gmail.com"><FaEnvelope/></a>
                         </div>
                     </div>
-                    <div className={classes.message}>
-                        <ContactsForm/>
-                    </div>
+                    <ContactsForm/>
                 </div>
             </div>
         </div>
