2c20f5ef3476c4289799ad759204ce0967961d47
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-29 11:41:40 +0300

    feat: wrap navigation links into Scrollspy, update styles

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -2,26 +2,20 @@ import React from 'react';
 import classes from './Nav.module.scss';
 import {AiOutlineUser} from 'react-icons/ai';
 import {BiBook, BiBriefcase, BiMessageDetail} from 'react-icons/bi';
+import Scrollspy from 'react-scrollspy';
 
-type NavType = {
-    activeLink: string
-    setActiveLink: (link: string) => void
-}
 
-export const Nav: React.FC<NavType> = ({activeLink, setActiveLink}) => {
-
-    const activeHandler = (link: string) => () => setActiveLink(link)
+export const Nav = () => {
 
     return (
-        <nav className={classes.nav}>
-            <a href="#main" onClick={activeHandler('#main')}
-               className={activeLink === '#main' ? classes.active : ''}><AiOutlineUser/></a>
-            <a href="#skills" onClick={activeHandler('#skills')}
-               className={activeLink === '#skills' ? classes.active : ''}><BiBook/></a>
-            <a href="#projects" onClick={activeHandler('#projects')}
-               className={activeLink === '#projects' ? classes.active : ''}><BiBriefcase/></a>
-            <a href="#contacts" onClick={activeHandler('#contacts')}
-               className={activeLink === '#contacts' ? classes.active : ''}><BiMessageDetail/></a>
+        <nav>
+            <Scrollspy items={['main', 'skills', 'projects', 'contacts']} currentClassName={classes.active}
+                       className={classes.nav}>
+                <a href="#main"><AiOutlineUser/></a>
+                <a href="#skills"><BiBook/></a>
+                <a href="#projects"><BiBriefcase/></a>
+                <a href="#contacts"><BiMessageDetail/></a>
+            </Scrollspy>
         </nav>
     );
 };
